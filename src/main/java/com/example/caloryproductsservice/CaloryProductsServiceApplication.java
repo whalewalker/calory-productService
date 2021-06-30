2889e3a6c21e74c0266dd9ca9a50da8a399cb5c9
whalewalker <mongodbms@gmail.com>
2021-06-30 09:45:04 +0100

    setup produts service and implement application config

---
+++ src/main/java/com/example/caloryproductsservice/CaloryProductsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.caloryproductsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloryProductsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaloryProductsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/caloryproductsservice/CaloryProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caloryproductsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloryProductsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
